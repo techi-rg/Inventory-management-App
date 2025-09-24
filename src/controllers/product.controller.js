33fc0064314ca388ca53f82cc57c7752b96b87e1
Raginee Vaid <ragineevaid5353@gmail.com>
2025-09-24 20:02:11 +0530

    new lines added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,9 +1,13 @@
 
 import path from 'path';
+import ProductModel from '../models/product.model.js';
+
 export default class ProductController
 {
     getProducts (req, res)
     {
+        let products = ProductModel.get();
+        console.log( products );
         res.sendFile(path.join(path.resolve(), "src","views", "products.html"));
     }
 }
